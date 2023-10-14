42610fdb7338d214311e51c076f8bccca3d896bc
Antony Kowalski <beheaded_96@mail.ru>
2023-10-14 21:35:41 +0700

    deploy ver. 1.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Tosoundra/airdrop-hunter',
+  base: '/airdrop-hunter/',
   plugins: [react()],
 });
